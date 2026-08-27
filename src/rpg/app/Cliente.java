2202daf8903dd11afcb78540a290cf351c5e2513
Renan Duque <42757763+reduq@users.noreply.github.com>
2026-08-27 16:53:49 -0300

    melhora comentários

--- src/rpg/app/Cliente.java
+++ src/rpg/app/Cliente.java
@@ -12,34 +12,28 @@ public class Cliente {
 			FabricaPersonagem fabrica = new FabricaGuerreiro();
 			
 			
-			// declara a variável de referência p1, do tipo Personagem (interface).
-			// através da referência armazenada em "fabrica" chama o método criaPersonagem(), implementado por 'FabricaGuerreiro'
-			// o método instancia um objeto 'Guerreiro' e retorna sua referência, que é armazenada na variável p1.
+			//declara a variável de referência guerreiro como um Personagem (interface).
+			//através da referência armazenada em "fabrica" chama o método criaPersonagem(), implementado por 'FabricaGuerreiro'
+			//o método instancia um objeto 'Guerreiro' e retorna sua referência, que é armazenada na variável guerreiro.
 			Personagem guerreiro = fabrica.criaPersonagem();
 			
 			//cliente seleciona a classe "Arqueiro" pelo setter
 			//instancia o objeto 'FabricaArqueiro' e armazena sua referência em 'fabrica'
 			fabrica = new FabricaArqueiro();
-			
-			// declara a variável de referência p2 do tipo Personagem (interface).
-			// através da referência armazenada em "fabrica" chama o método criaPersonagem(), implementado por 'FabricaArqueiro'
-			// o método instancia um objeto 'Arqueiro' e retorna sua referência, que é armazenada na variável p2.
 			Personagem arqueiro = fabrica.criaPersonagem();
-			
-			//cliente seleciona a classe "Feiticeiro" pelo setter
-			//instancia o objeto 'FabricaFeiticeiro' e armazena sua referência em 'fabrica'
+
 			fabrica = new FabricaFeiticeiro();
-			
-			// declara a variável de referência p3 do tipo Personagem (interface).
-			// através da referência armazenada em "fabrica" chama o método criaPersonagem(), implementado por 'FabricaFeiticeiro'
-			// o método instancia um objeto 'Feiticeiro' e retorna sua referência, que é armazenada na variável p3.
 			Personagem feiticeiro = fabrica.criaPersonagem();
-			
+
+			//declara uma referência pra interface das nossas ações
 			Command c;
 			
 			System.out.println("\n==TURNO DO ARQUEIRO==");
+			//instancia um command concreto de ataque para o personagem arqueiro e armazena a referência em "c"
 			c = new AtaqueCommand(arqueiro);
+			//chama o método de ação dentro do command do ataque, que por sua vez chama o método atacar dentro do objeto arqueiro
 			c.acao();
+			
 			System.out.println("\n==TURNO DO GUERREIRO==");
 			c = new DefesaCommand(guerreiro);
 			c.acao();
